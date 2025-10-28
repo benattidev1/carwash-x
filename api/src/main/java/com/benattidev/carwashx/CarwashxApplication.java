112bb0afa40be25fff12b3c997a6b62d6f102bce
Gabriel Wisky Benatti <benattidev@gmail.com>
2025-10-28 19:08:40 -0300

    build: iniciar projeto java e string boot

---
+++ api/src/main/java/com/benattidev/carwashx/CarwashxApplication.java
@@ -0,0 +1,13 @@
+package com.benattidev.carwashx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarwashxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarwashxApplication.class, args);
+	}
+
+}
