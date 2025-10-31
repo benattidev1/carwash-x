6a5ced73ebfa1bad9ebb28fb6dbcd85ba6988e65
Gabriel Wisky Benatti <benattidev@gmail.com>
2025-10-31 11:48:58 -0300

    feat(tenant): criar repositório para Tenant

---
+++ api/src/main/java/com/benattidev/carwashx/modules/tenant/domain/repository/TenantRepository.java
@@ -0,0 +1,18 @@
+package com.benattidev.carwashx.modules.tenant.domain.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.benattidev.carwashx.modules.tenant.domain.Tenant;
+
+@Repository
+public interface TenantRepository extends JpaRepository<Tenant, UUID> {
+
+    Optional<Tenant> findByTaxId(String taxId);
+
+    Optional<Tenant> findByEmail(String email);
+
+}
